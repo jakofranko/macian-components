cd0c4fb9971c62152e2f33dd9896299deab9e0f4
Jake Franklin <jacob.d.franklin@gmail.com>
2023-11-10 11:26:23 -0700

    Create Button.js

---
+++ src/Button.js
@@ -0,0 +1,19 @@
+import m from 'mithril';
+
+export default const Button = {
+  view: function(vnode) {
+    const { attrs: { buttonType, onclick } } = vnode;
+    let classes = 'm2 p4 br2';
+    
+    if (buttonType === 'secondary') {
+      classes += ' bg-blanc blu ba bw1 b-blu';
+    } else {
+      classes += ' bg-blu blanc';
+    }
+    
+    return m('button', {
+      class: classes,
+      onclick
+    });
+  }
+}
